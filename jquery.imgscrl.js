8b46ab72ce45ddff1f677a8126832c977ee1b2eb
Martijn Grendelman <martijn@grendelman.net>
2013-01-31 11:36:25 +0100

    Support multiple images on one page and maintain chainability

--- jquery.imgscrl.js
+++ jquery.imgscrl.js
@@ -1,64 +1,69 @@
 (function($){
 
 	$.imgscrl = function() {
-		var containerWidth = 0;
-		var imgWidth = 0;
 	}
 
 	$.fn.imgscrl = function(width, options) {
 
-		if (typeof(width) == 'undefined') {
-			width = 640;
-		}
+		return this.each(function() {
 
-		var settings = $.extend( {
-			'class': ''
-		}, options);
+			var $this = $(this);
 
-		// wrap the image in a div, giving it an optional extra class
-		extraClass = '';
-		if (settings.class) {
-			extraClass = ' ' + settings.class;
-		}
-		this.wrap('<div class="imgscrl_div' + extraClass + '" />');
+			if (typeof(width) == 'undefined') {
+				width = 640;
+			}
 
-		// format the div
-		$('.imgscrl_div').css({
-			'width': width + 'px',
-			'overflow': 'hidden',
-		});
+			var settings = $.extend( {
+				'class': ''
+			}, options);
 
-		// display the image in case it was hidden before
-		$(this).css({
-			'margin': 0,
-			'display': 'block',
-			'max-width': 'none'
-		});
+			// wrap the image in a div, giving it an optional extra class
+			extraClass = '';
+			if (settings.class) {
+				extraClass = ' class="' + settings.class + '"';
+			}
+			var uniq = Math.floor( Math.random()*99999 );
+			var div_id = "imgscrl_div" + String(uniq);
+			$this.wrap('<div id="' + div_id + '"' + extraClass + ' />');
 
-		// Check if image has dimensions; if not, it's probably not yet loaded.
-		var deferred = $.Deferred();
-		if (this.width() == 0)
-			this[0].onload = deferred.resolve;
-		else
-			deferred.resolve();
-
-		deferred.promise().then(function() {
-			$.imgscrl.containerWidth = width;
-			$.imgscrl.imgWidth = $(this).outerWidth(true);
-
-			this.mousemove(function(event){
-				offset = $(this).parent().offset()
-				var x = event.pageX - offset.left;
-
-				var perc = (100 / ($.imgscrl.containerWidth / x));
-				$.imgscrl.posPicture($(this),perc);
+			// format the div
+			$('#' + div_id).css({
+				'width': width + 'px',
+				'overflow': 'hidden',
 			});
-		}.bind(this));
+
+			// display the image in case it was hidden before
+			$(this).css({
+				'margin': 0,
+				'display': 'block',
+				'max-width': 'none'
+			});
+
+			// Check if image has dimensions; if not, it's probably not yet loaded.
+			var deferred = $.Deferred();
+			if ($this.width() == 0)
+				$this[0].onload = deferred.resolve;
+			else
+				deferred.resolve();
+
+			deferred.promise().then(function() {
+				$this.data('containerWidth', width);
+				$this.data('imgWidth', $(this).outerWidth(true));
+
+				$this.mousemove(function(event){
+					offset = $(this).parent().offset()
+					var x = event.pageX - offset.left;
+
+					var perc = (100 / ($(this).data('containerWidth') / x));
+					$.imgscrl.posPicture($(this),perc);
+				});
+			}.bind($this));
+		});
 	}
 
 	$.imgscrl.posPicture = function(p,x) {
-		var full = $.imgscrl.imgWidth;
-		full = full - $.imgscrl.containerWidth;
+		var full = p.data('imgWidth');
+		full = full - p.data('containerWidth');
 		var curX = full * (x / 100);
 
 		if (curX < 0) {
